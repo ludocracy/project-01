7e0c25777bb886450a5bb115e0e0962e6f1a87ba
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-10-16 12:36:34 -0500

    populated users and songs

--- routes/playlists.js
+++ routes/playlists.js
@@ -2,24 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const db = require('../models');
 
-//create playlist and returns playlist as JSON object
-//TODO: integrate with User.createPlaylist route
-function createPlaylist(req, res){
-  const newPlaylist = db.Playlist ({
-    name: req.body.name,
-    description: req.body.description,
-  });
-
-  newPlaylist.save(function(err, data){
-    if (err){
-      console.log('Error creating playlist.', err);
-      res.status(500).send('Internal server error.');
-    }else{
-      res.status(201).json(data);
-    };
-  });
-};
-
 //Retrieves a playlist and returns them in a JSON object
 function getOnePlaylist(req, res){
   db.Playlist.findById(req.params.id, function(err, data){
@@ -32,19 +14,9 @@ function getOnePlaylist(req, res){
   });
 };
 
-function getAllPlaylists(req, res){
-  db.Playlist.find({}, function(err, data){
-    if(err){
-      console.log('Error retrieving this playlist.', err);
-      res.status(500).send('Internal server error.');
-    }else{
-      res.json(data);
-    };
-  });
-};
 
 //Retrieves the contributors to a playlist TODO: INTEGRATE USERS TO PLAYLIST SCHEMA
-function getAllUsers(req, res){
+function getAllContributors(req, res){
   db.Playlist.findById(req.params.id, function(err, data){
     if(err){
       console.log('Error retrieving the constributors of this playlist.', err);
@@ -64,9 +36,9 @@ function updatePlaylist(req, res){
     }else{
       data.name = req.body.name || data.name;
       data.description = req.body.description || data.description;
-      data.songs = req.body.songs || data.songs;
-      data.save(data);
-      res.json(data);
+      data.save(function(err, savedData){
+        res.json(savedData);
+      });
     };
   });
 };
@@ -93,10 +65,8 @@ function deletePlaylist(req, res){
 };
 
 module.exports = {
-  getAllUsers: getAllUsers,
-  createPlaylist: createPlaylist,
+  getAllContributors: getAllContributors,
   getOnePlaylist: getOnePlaylist,
   deletePlaylist: deletePlaylist,
-  updatePlaylist: updatePlaylist,
-  getAllPlaylists: getAllPlaylists
+  updatePlaylist: updatePlaylist
 };
