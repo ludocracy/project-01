322213b5bf8f2bbff3788b467713e24cbfa10d47
Peter Kong <bansuki@gmail.com>
2017-10-19 16:37:40 -0500

    fixed song retrieval bug

--- routes/playlists.js
+++ routes/playlists.js
@@ -34,7 +34,8 @@ function createPlaylist(req, res){
 
 //Retrieves a playlist and returns them in a JSON object
 function getOnePlaylist(req, res){
-  db.Playlist.findById(req.params.id, function(err, data){
+  db.Playlist.findById(req.params.id)
+  .populate('songs').exec(function(err, data){
     if(err){
       console.log('Error retrieving this playlist.', err);
       res.status(500).send('Internal server error.');
